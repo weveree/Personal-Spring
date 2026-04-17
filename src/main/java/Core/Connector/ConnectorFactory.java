dda5d1ba751b31ba843de53853cab265baa3029a
weveree <wahil.boulahbach.blog@gmail.com>
2026-04-17 21:36:46 +0200

    Improve structure

--- src/main/java/Core/Connector/ConnectorFactory.java
+++ src/main/java/Core/Connector/ConnectorFactory.java
@@ -6,20 +6,19 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class ConnectorFactory implements IConnector {
+public class ConnectorFactory{
     public static Connection connection;
 
-    public static Connection postgres(String host,int port,String db,String user,String password) throws SQLException {
+    private static Connection postgres(String host,int port,String db,String user,String password) throws SQLException {
         String url = String.format("jdbc:postgresql://%s:%d/%s", host, port, db);
         connection=DriverManager.getConnection(url,user,password);
         return connection;
     }
 
-    @Override
-    public Connection connect(String type, String host, int port, String db, String user, String password) throws SQLException, UnknownDatabaseTypeException {
+    public static Connection connect(String type, String host, int port, String db, String user, String password) throws SQLException, UnknownDatabaseTypeException {
         DatabaseType dbType;
         try {
-            dbType = DatabaseType.valueOf(type);
+            dbType = DatabaseType.valueOf(type.toUpperCase());
         }
         catch (IllegalArgumentException e)
         {
